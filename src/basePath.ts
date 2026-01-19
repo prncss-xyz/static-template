86c6a1530ac897b1b52147eb019cf68bdea46125
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-01-18 22:58:11 -0500

    fix: css

--- src/basePath.ts
+++ src/basePath.ts
@@ -1,4 +1,18 @@
+/*
 export const basePath = import.meta.env.VITE_BASE_PATH
 	? '/' + import.meta.env.VITE_BASE_PATH + '/'
 	: '/'
+*/
+
+console.log(import.meta.env.VITE_BASE_PATH)
+
+function getBasePath() {
+	const repo = import.meta.env.VITE_GITHUB_REPOSITORY
+	if (!repo) return '/'
+	const [, name] = repo.split('/')
+	if (!name) return undefined
+	return '/' + name + '/'
+}
+
+export const basePath = getBasePath()
 export const baseUrl = import.meta.env.VITE_BASE_URL ?? 'http://localhost:3000'
