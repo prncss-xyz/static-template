fee49f72e72da26992daf9f3d6a3258b21a21524
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-01-18 22:47:51 -0500

    fix: env variable

--- src/basePath.ts
+++ src/basePath.ts
@@ -1,2 +1,4 @@
 export const basePath = import.meta.env.VITE_BASE_PATH
-export const baseUrl = import.meta.env.VITE_BASE_URL
+	? '/' + import.meta.env.VITE_BASE_PATH + '/'
+	: '/'
+export const baseUrl = import.meta.env.VITE_BASE_URL ?? 'http://localhost:3000'
